07adcbfe886fe92ec09d1cd8e5b818a951b0c5d0
karlaechartea <k.echartea@chicagobooth.edu>
2024-12-12 23:11:53 +0000

    initialsetup

---
+++ db/migrate/20241212224858_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.integer :comments_count
+      t.integer :likes_count
+      t.boolean :private
+
+      t.timestamps
+    end
+  end
+end
