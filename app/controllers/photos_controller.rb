01b30cd622577cecf2ecfc972038b0cdb026911c
karlaechartea <k.echartea@chicagobooth.edu>
2024-12-13 08:00:30 +0000

    image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -25,8 +25,8 @@ class PhotosController < ApplicationController
     the_photo.caption = params.fetch("query_caption")
     the_photo.comments_count = params.fetch("query_comments_count")
     the_photo.likes_count = params.fetch("query_likes_count")
-    the_photo.owner_id = params.fetch("query_owner_id")
-    the_photo.image = params.fetch("query_image")
+    the_photo.owner_id = current_user.id if user_signed_in?
+    the_photo.image = params.fetch("image") if params.key?("image")
 
     if the_photo.valid?
       the_photo.save
@@ -43,8 +43,9 @@ class PhotosController < ApplicationController
     the_photo.caption = params.fetch("query_caption")
     the_photo.comments_count = params.fetch("query_comments_count")
     the_photo.likes_count = params.fetch("query_likes_count")
-    the_photo.owner_id = params.fetch("query_owner_id")
-    the_photo.image = params.fetch("query_image")
+    the_photo.owner_id = current_user.id if user_signed_in?
+    the_photo.image = params.fetch("image") if params.key?("image")
+
 
     if the_photo.valid?
       the_photo.save
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
+require "carrierwave"
+require "carrierwave/orm/activerecord"
+require "cloudinary"
+require "carrierwave/storage/cloudinary"
 
 # Initialize the Rails application.
 Rails.application.initialize!
