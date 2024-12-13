cbc12926aa5ecd37a51bfcfb7b6516d50333cfa7
karlaechartea <k.echartea@chicagobooth.edu>
2024-12-13 05:40:35 +0000

    users home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
 
-    @list_of_users = matching_users.order({ :created_at => :desc })
+    @users = matching_users.order({ :created_at => :desc })
 
     render({ :template => "users/index" })
   end
